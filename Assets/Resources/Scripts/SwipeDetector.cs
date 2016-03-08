d1ba6a54ac70d4890e0460f17b03e48aaa2d2c49
Simon Ever-Hale <severhal@oberlin.edu>
2016-03-07 21:07:41 -0500

    Fixed swipe detector

--- Assets/Resources/Scripts/SwipeDetector.cs
+++ Assets/Resources/Scripts/SwipeDetector.cs
@@ -5,10 +5,10 @@ public class SwipeDetector : MonoBehaviour {
 	Vector3 startPos = Vector3.zero;
 	const int minSwipeDistX = 50;
 	const int minSwipeDistY = 50;
+	bool movedOnThisSwipe = false;
 
 	public Vector2 getSwipeDirection() {
 		Vector2 result = Vector2.zero;
-		bool movedOnThisSwipe = false;
 		if (Input.touchCount > 0) {
 			Touch touch = Input.touches[0];
 			switch (touch.phase) {
@@ -23,32 +23,34 @@ public class SwipeDetector : MonoBehaviour {
 					movedOnThisSwipe = false;
 					break;
 				case TouchPhase.Moved:
-					print("New position: " + touch.position);
-					Vector2 pos = touch.position;
-					if (Mathf.Abs(startPos.x - pos.x) >= minSwipeDistX) {
-						movedOnThisSwipe = true;
-						if (Mathf.Sign(startPos.x - pos.x) > 0) {
-							// right swipe
-							return Vector2.right;
+					if (!movedOnThisSwipe) {
+						print("New position: " + touch.position);
+						Vector2 pos = touch.position;
+						if (Mathf.Abs(startPos.x - pos.x) >= minSwipeDistX) {
+							movedOnThisSwipe = true;
+							if (Mathf.Sign(startPos.x - pos.x) > 0) {
+								// right swipe
+								return Vector2.right;
+							}
+							else {
+								// left swipe;
+								return Vector2.left;
+							}
+						}
+						else if (Mathf.Abs(startPos.y - pos.y) >= minSwipeDistY) {
+							movedOnThisSwipe = true;
+							if (Mathf.Sign(startPos.y - pos.y) > 0) {
+								// up swipe
+								return Vector2.up;
+							}
+							else {
+								// down swipe
+								return Vector2.down;
+							}
 						}
 						else {
-							// left swipe;
-							return Vector2.left;
+							return Vector2.zero;
 						}
-					}
-					else if (Mathf.Abs(startPos.y - pos.y) >= minSwipeDistY) {
-						movedOnThisSwipe = true;
-						if (Mathf.Sign(startPos.y - pos.y) > 0) {
-							// up swipe
-							return Vector2.up;
-						}
-						else {
-							// down swipe
-							return Vector2.down;
-						}
-					}
-					else {
-						return Vector2.zero;
 					}
 					break;
 			}
