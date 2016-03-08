f2987749cee9cdf4c68f6846a02b390497804906
Simon Ever-Hale <severhal@oberlin.edu>
2016-03-07 20:52:06 -0500

    Added possible touch screen capabilities
    Can't test yet

---
+++ Assets/Resources/Scripts/SwipeDetector.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeDetector : MonoBehaviour {
+	Vector3 startPos = Vector3.zero;
+	const int minSwipeDistX = 50;
+	const int minSwipeDistY = 50;
+
+	public Vector2 getSwipeDirection() {
+		Vector2 result = Vector2.zero;
+		bool movedOnThisSwipe = false;
+		if (Input.touchCount > 0) {
+			Touch touch = Input.touches[0];
+			switch (touch.phase) {
+				case TouchPhase.Began:
+					print("Started new swipe");
+					startPos = touch.position;
+					movedOnThisSwipe = false;
+					break;
+				case TouchPhase.Stationary:
+					print("Started new swipe");
+					startPos = touch.position;
+					movedOnThisSwipe = false;
+					break;
+				case TouchPhase.Moved:
+					print("New position: " + touch.position);
+					Vector2 pos = touch.position;
+					if (Mathf.Abs(startPos.x - pos.x) >= minSwipeDistX) {
+						movedOnThisSwipe = true;
+						if (Mathf.Sign(startPos.x - pos.x) > 0) {
+							// right swipe
+							return Vector2.right;
+						}
+						else {
+							// left swipe;
+							return Vector2.left;
+						}
+					}
+					else if (Mathf.Abs(startPos.y - pos.y) >= minSwipeDistY) {
+						movedOnThisSwipe = true;
+						if (Mathf.Sign(startPos.y - pos.y) > 0) {
+							// up swipe
+							return Vector2.up;
+						}
+						else {
+							// down swipe
+							return Vector2.down;
+						}
+					}
+					else {
+						return Vector2.zero;
+					}
+					break;
+			}
+		}
+		return Vector2.zero;
+	}
+}
+
